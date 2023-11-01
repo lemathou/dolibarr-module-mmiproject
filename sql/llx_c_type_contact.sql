865ba8b1349f9b45e23078d5f17eea85f38d8363
Mathieu Moulin <mathieu@iprospective.fr>
2023-11-01 13:04:45 +0100

    Project management display

---
+++ sql/llx_c_type_contact.sql
@@ -0,0 +1,6 @@
+INSERT IGNORE INTO `llx_c_type_contact`
+(`element`, `source`, `code`, `libelle`, `active`, `module`, `position`)
+VALUES
+('propal', 'external', 'MGMT', 'Project Management', 1, 'MMIProject', 10),
+('commande', 'external', 'MGMT', 'Project Management', 1, 'MMIProject', 10),
+('facture', 'external', 'MGMT', 'Project Management', 1, 'MMIProject', 10);
